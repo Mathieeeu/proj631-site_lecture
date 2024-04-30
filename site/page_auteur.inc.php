a64438f4a308a7fdb50833cf3a2e94842a5ba898
Mathieu le ratpi <77482141+Mathieeeu@users.noreply.github.com>
2024-04-30 21:52:09 +0200

    corrections

--- site/page_auteur.inc.php
+++ site/page_auteur.inc.php
@@ -1,10 +1,29 @@
 <link rel="stylesheet" href="page_auteur.inc.css"/>
 
 <?php
-$sql = "SELECT * FROM auteur";
+/*
+    Format table 'PROJ631_auteur':
+    id_auteur INT,
+    nom_prenom_pseudo VARCHAR,
+    date_naissance DATE,
+    date_deces DATE,
+    biographie TEXT,
+    photo VARCHAR,
+*/
+$sql = "SELECT * FROM PROJ631_auteur";
 $result = mysqli_query($conn, $sql);
-$val = mysqli_fetch_assoc($result);
 
-echo "<b>Les auteurs</b><br>";
-echo "<table style='min-width:500px;text-align:center'><tr><th>Photo</th><th>Auteur</th><th>Dates</th><th>Biographie</th></tr>";
+echo "<h2>Les auteurs</h2><br>";
+echo "<div id='table_auteurs'><table style='min-width:500px;text-align:center'><tr><th>Photo</th><th>Auteur</th><th>Dates</th><th>Biographie</th></tr>";
+
+while($val = mysqli_fetch_assoc($result)){
+    echo "<tr><td><img src='".$val['photo']."' alt='alt: ".$val['nom_prenom_pseudo']."' style='max-width:100px;max-height:100px'></td>";
+    echo "<td>".$val['nom_prenom_pseudo']."</td>";
+    $annee_naissance = date('Y', strtotime($val['date_naissance']));
+    $annee_deces = date('Y', strtotime($val['date_deces']));
+    echo "<td>".$annee_naissance." - ".$annee_deces."</td>";
+    echo "<td>".$val['biographie']."</td></tr>";
+}
+echo "</table></div>"
+
 ?>
