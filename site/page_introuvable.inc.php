fb893102d739a9029f69d5034f4505311ae926d7
Mathieu <77482141+Mathieeeu@users.noreply.github.com>
2024-04-30 23:08:20 +0200

    Add files via upload

---
+++ site/page_introuvable.inc.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="css/page_introuvable.inc.css"/>
+
+<?php
+echo "<div id='error-message'>";
+echo "<img src='../images/sad.png' alt='sad face'>";
+echo "<p>Erreur :(<br>Page inexistante...</p>";
+echo "</div>";
+?>
