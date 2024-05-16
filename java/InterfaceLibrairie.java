2aedcfe71bd34b779d9b0b35a52f330308815c63
Axelle <160240449+aquellery@users.noreply.github.com>
2024-05-16 16:36:10 +0200

    Modification d'une donnée avec mise à jour de l'interface

--- java/InterfaceLibrairie.java
+++ java/InterfaceLibrairie.java
@@ -6,14 +6,14 @@ import java.util.ArrayList;
 import java.awt.*;
 
 public class InterfaceLibrairie extends JFrame {
-    final static int HAUTEUR = 550;
-    final static int LARGEUR = 700;
+    final static int HAUTEUR = 600;
+    final static int LARGEUR = 900;
     private ArrayList<Table> liste_tables;
     private Table table_selectionnee;
     private Donnees donnee_selectionnee;
-    private JPanel fond;
     private JPanel affichage;
     private JPanel zone_ajout;
+    private JPanel zone_modification;
     private JComboBox<String> choix_table;
 
     public InterfaceLibrairie(ArrayList<Table> liste_tables) {
@@ -55,9 +55,10 @@ public class InterfaceLibrairie extends JFrame {
         JPanel actions=new JPanel();
         Color fond_boutons=Color.decode("#74a892");
         Color couleur_boutons=Color.decode("#008585");
-
         actions.setBackground(fond_boutons);
+        // AJOUTER UNE DONNEE
         JButton ajouter=new JButton("Ajouter");
+        ajouter.setForeground(couleur_boutons);
         ajouter.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -105,31 +106,86 @@ public class InterfaceLibrairie extends JFrame {
                 });
                 zone_ajout.add(valider);               
                 fond.add(zone_ajout);
-               
-                // mise à jour de l'affichage
-                mise_a_jour_interface();
+                mise_a_jour_affichage();
             }
         });
-        ajouter.setForeground(couleur_boutons);
+        
 
+        //MODIFIER UNE DONNEE
         JButton modifier=new JButton("Modifier");
         modifier.setForeground(couleur_boutons);
         modifier.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                if (donnee_selectionnee==null){
+                    System.out.println("Aucune donnée sélectionnée");
+                }
+                else{
+                    System.out.println("Modification de la donnée "+donnee_selectionnee.affichage_donnee()+" en cours");
+                    // affichage de la modification de la donnée
+                    zone_modification=new JPanel();
+                    zone_modification.setLayout(new BoxLayout(zone_modification, BoxLayout.Y_AXIS));
+                    Color couleur_modification=Color.decode("#74a892");
+                    zone_modification.setBackground(couleur_modification);
 
+                    // affichage en 2 zones principales
+                    // zone choix : boutons qui représentes les colonnes de la donnée
+                    JPanel zone_choix=new JPanel();
+                    zone_choix.setBackground(couleur_modification);
+                    // zone entrée : modification de la donnée et validation
+                    JPanel zone_entree=new JPanel();
+                    zone_entree.setBackground(couleur_modification);
+
+                    // on affiche toutes les colonnes de la table pour sélectionner celle qui doit être modifiée
+                    for (String nomDonnee : donnee_selectionnee.getContenu()){
+                        System.out.println("Dans la boucle");
+                        JButton choix_colonne=new JButton("<html>"+nomDonnee+"</html>");
+                        choix_colonne.setForeground(couleur_boutons);
+                        // chaque bouton de choix est ajouté à la zone de choix
+                        zone_choix.add(choix_colonne);
+                        choix_colonne.addActionListener(new ActionListener(){
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                // on affiche un JTextField pour permettre de modifier l'élément de la donnée
+                                System.out.println("Une colonne a été sélectionnée");
+                                JTextField modification=new JTextField(nomDonnee);
+                                zone_entree.add(modification);
+                                // on récupère l'indexe du nom de la colonne dans la table
+                                int index=donnee_selectionnee.getContenu().indexOf(nomDonnee);
+                                JButton valider=new JButton("Valider");
+                                zone_entree.add(valider);
+                                valider.addActionListener(new ActionListener() {
+                                    @Override
+                                    public void actionPerformed(ActionEvent e) {
+                                        //on récupère le texte qui a été entré
+                                        String donnee_modifiee=modification.getText();
+                                        System.out.println("Modification de "+nomDonnee+" par "+donnee_modifiee+" en cours.");
+                                        table_selectionnee.modifierDonnee(nomDonnee, donnee_modifiee, index);
+                                        suppression_zone_modification();
+                                        };
+                                    });
+                                    mise_a_jour_affichage(); 
+                                };
+                            });   
+                        zone_modification.add(zone_choix);
+                        zone_modification.add(zone_entree);                            
+                    } 
+                }
+                fond.add(zone_modification);
+                mise_a_jour_affichage(); 
             }
         });
 
+        //SUPPRIMER UNE DONNEE
         JButton supprimer=new JButton("Supprimer");
         supprimer.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Supprimer la donnée sélectionnée
-                System.out.println("Suppression de la donnée " + donnee_selectionnee.affichage_donnee() + " en cours");
+                System.out.println("Suppression de la donnée "+donnee_selectionnee.affichage_donnee()+" en cours");
                 table_selectionnee.supprimerDonnee(donnee_selectionnee);
                 // mise à jour de l'affichage
-                mise_a_jour_interface();
+                mise_a_jour_affichage();
             }
         });
         supprimer.setForeground(couleur_boutons);
@@ -141,7 +197,7 @@ public class InterfaceLibrairie extends JFrame {
         // création de la zone d'affichage des données
         affichage=new JPanel();
         affichage.setBackground(couleur_fond);
-        affichage.setPreferredSize(new Dimension(680, 380));
+        affichage.setPreferredSize(new Dimension(700, 380));
         affichage.setLayout(new BoxLayout(affichage, BoxLayout.Y_AXIS));
         Color couleur_bordure=Color.decode("#c7522a");
         Border border=BorderFactory.createLineBorder(couleur_bordure, 5);
@@ -162,7 +218,6 @@ public class InterfaceLibrairie extends JFrame {
                 }
             });
             affichage.add(bouton_donnee);
-
         }
 
         choix_table.addActionListener(new ActionListener() {
@@ -174,7 +229,7 @@ public class InterfaceLibrairie extends JFrame {
                 for (Table t : liste_tables) {
                     if (t.getNom().equals(selection_table)) {
                         table_selectionnee = t;
-                        mise_a_jour_interface();
+                        mise_a_jour_affichage();
                     }
                 }
             }
@@ -189,13 +244,13 @@ public class InterfaceLibrairie extends JFrame {
         return res;
     }
 
-    private void mise_a_jour_interface() {
-        System.out.println("Mise à jour de l'interface en cours");
+    private void mise_a_jour_affichage() {
+        System.out.println("Mise à jour de l'affichage des données");
         // partie affichage des données
         ArrayList<Donnees> liste_donnees = this.table_selectionnee.getDonnees();
         affichage.removeAll();
         for (Donnees donnee : liste_donnees) {
-            JButton bouton_donnee=new JButton("<html>" + donnee.affichage_interface() + "</html>");
+            JButton bouton_donnee=new JButton("<html>"+donnee.affichage_interface()+"</html>");
             bouton_donnee.setOpaque(false);
             bouton_donnee.setContentAreaFilled(false);
             bouton_donnee.addActionListener(new ActionListener() {
@@ -211,30 +266,26 @@ public class InterfaceLibrairie extends JFrame {
         // mise à jour de l'affichage des données
         affichage.revalidate();
         affichage.repaint();
-        
+    }
+
+    private void suppression_zone_modification(){
+        System.out.println("Mise à jour de l'interface en cours");
+        // partie affichage des données
+        mise_a_jour_affichage();
+
+        zone_modification.removeAll();
+        Color couleur_fond=Color.decode("#fbf2c4");
+        zone_modification.setBackground(couleur_fond);
+        zone_modification.revalidate();
+        zone_modification.repaint();
     }
 
     private void suppression_zone_ajout(){
         System.out.println("Mise à jour de l'interface en cours");
         // partie affichage des données
-        ArrayList<Donnees> liste_donnees = this.table_selectionnee.getDonnees();
-        affichage.removeAll();
-        for (Donnees donnee : liste_donnees) {
-            JButton bouton_donnee=new JButton("<html>" + donnee.affichage_interface() + "</html>");
-            bouton_donnee.setOpaque(false);
-            bouton_donnee.setContentAreaFilled(false);
-            bouton_donnee.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    // On récupère la donnée sélectionnee
-                    donnee_selectionnee=donnee;
-                    System.out.println("Donnée sélectionnée : " + donnee_selectionnee.affichage_donnee());
-                }
-            });            
-            affichage.add(bouton_donnee);
-        }
+        mise_a_jour_affichage();
+
         // partie pour la zone d'ajout d'une donnée
-        
         //mise à jour de l'affichage pour masquer la zone d'ajout d'une donnée
         zone_ajout.removeAll();
         Color couleur_fond=Color.decode("#fbf2c4");
