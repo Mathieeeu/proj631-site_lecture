9f76df8886825b03b2ed0d114e7348b0c673ded8
CypGttn <154144790+CypGttn@users.noreply.github.com>
2024-05-16 15:05:36 +0200

    Recherche fonctionnelle
    + Boxes d'affichage créées

--- site/page_accueil.inc.php
+++ site/page_accueil.inc.php
@@ -1,10 +1,17 @@
-<div>
-<form action="" method="get">
-  <input type="text" name="q" placeholder="Rechercher...">
-  <button type="submit">Rechercher</button>
-</form>
-<div>
+<link rel="stylesheet" href="css/page_accueil.inc.css"/>
+
 <?php
+    echo "<div>";
+    echo "<form action='' method='get' class='search'>";
+    if (isset($_GET["q"])) {
+        echo "<input type='text' name='q' placeholder='Rechercher...' value = ". $_GET["q"] . ">  </input>";
+    }
+    else {
+        echo "<input type='text' name='q' placeholder='Rechercher...'></input>";
+    }
+    echo "<button type='submit'>Rechercher</button>";
+    echo "</form>";
+    echo "<div>";
     if (isset($_GET["q"])) {
         // Récupérer la requête de recherche
         $q = $_GET["q"];        
@@ -19,11 +26,22 @@
             mysqli_query($conn, "SET NAMES UTF8");
         }
         // Effectuer une recherche dans la base de données
-        $requete = "SELECT * FROM PROJ631_livre WHERE titre LIKE '" .$q."%'";
-        $result = mysqli_query($conn, $requete);
+        $requete_livre = "SELECT * FROM PROJ631_livre WHERE titre LIKE '%" .$q."%'";
+        $result_livre = mysqli_query($conn, $requete_livre);
         // Afficher les résultats de la recherche
-        while ($row = mysqli_fetch_assoc($result)) {
+        while ($row = mysqli_fetch_assoc($result_livre)) {
+            echo '<diV class=livre>';
             echo '<p>' . $row['titre'] . '</p>';
+            echo '</div>';
+        }
+        //Recherche les auteurs
+        $requete_auteur = "SELECT * FROM PROJ631_auteur WHERE nom_prenom_pseudo LIKE '%" .$q."%'";
+        $result_auteur = mysqli_query($conn, $requete_auteur);
+        // Afficher les résultats de la recherche
+        while ($row2 = mysqli_fetch_assoc($result_auteur)) {
+            echo '<diV class=auteur>';
+            echo '<p>' . $row2['nom_prenom_pseudo'] . '</p>';
+            echo '</div>';
         }
     }
 ?>
