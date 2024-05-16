a547dd409062e79c8a8ad0db6e2a8dcb10b3067b
Louna Camas <lounacamas@gmail.com>
2024-05-16 10:08:06 +0200

    connexion css

--- site/page_inscription.inc.php
+++ site/page_inscription.inc.php
@@ -2,8 +2,8 @@
     echo "<div class='containter'><h1>Inscription</h1>";
     echo "<form action='?page=accueil' method='post'>";
     echo "<input type='text' name='id' placeholder='Identifiant' required><br/>";
-    echo "<input type='text' name='mdp1' placeholder='Mot de passe' required><br>";
-    echo "<input type='text' name='mdp2' placeholder='Confirmer mot de passe' required><br>";
+    echo "<input type='password' name='mdp1' placeholder='Mot de passe' required><br>";
+    echo "<input type='password' name='mdp2' placeholder='Confirmer mot de passe' required><br>";
     echo "<input type='submit' value='Valider'>";
     echo "</div>";
 ?>
