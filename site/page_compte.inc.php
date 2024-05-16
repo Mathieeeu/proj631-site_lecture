da15537f4b9b020c006e364942f4e0767888287c
PULCINI-Livio <pulcinilivio04@gmail.com>
2024-05-16 09:54:00 +0200

    ajout avis et wishlist + debut css

--- site/page_compte.inc.php
+++ site/page_compte.inc.php
@@ -1,6 +1,17 @@
-<style>
+<head>
+  <link rel="stylesheet" type="text/css" href="css/page_compte.css">
+</head>
 
-</style>
+<?php 
+    $logs = file("../logs.txt");
+    $conn = @mysqli_connect("tp-epua:3308", substr($logs[0],0,-2), substr($logs[1],0,-2));
+    if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    } else {
+        mysqli_select_db($conn, substr($logs[0],0,-2));
+        mysqli_query($conn, "SET NAMES UTF8");
+    }
+?>
 
 <?php
 
@@ -25,10 +36,9 @@ echo "
     </div> 
 ";
 
-
+echo "<div id='btn_avis_wishlist'>";
 // Bouton wishlist et avis
 echo "
-    <div> 
         <form action='' method='get'>
         <input type='hidden' name='type_list' value='wishlist'>
         <button type='submit'>WISHLIST</button>
@@ -36,20 +46,55 @@ echo "
         <form action='' method='get'>
         <input type='hidden' name='type_list' value='avis'>
         <button type='submit'>AVIS</button>
-        </form>
-    </div> 
-";
+        </form>";
+
+echo "</div>";
+
+// Requetes pour la wishlist
+
+//$sql = "SELECT l.titre, l.resume, l.annee_parution, l.genre, l.image FROM PROJ631_livre as l
+//        JOIN PROJ631_wishlist as w ON l.id_livre = w.id_livre
+//        WHERE id_utilisateur = ".$_SESSION['id_utilisateur'].";"
+$sql = "SELECT l.titre, l.resume, l.annee_parution, l.genre, l.image FROM PROJ631_livre as l
+        JOIN PROJ631_wishlist as w ON l.id_livre = w.id_livre
+        WHERE id_utilisateur = 1";
+
+$result_wishlist = mysqli_query($conn, $sql) or die("Requête invalide: ". mysqli_error( $conn )."\n".$sql);
+//$val_wishlist = mysqli_fetch_assoc($result_wishlist);
+
+// Requetes pour les avis
+$sql = "SELECT note, commentaire FROM PROJ631_avis";
+$result_avis = mysqli_query($conn, $sql) or die("Requête invalide: ". mysqli_error( $conn )."\n".$sql);
+//$val_avis = mysqli_fetch_assoc($result_avis);
+
+
+
 
 // Affichage boutons wishlist et avis
 if(isset( $_GET["type_list"])) {
     if ($_GET["type_list"] == "wishlist") {
-        echo "
-        Vous verrez apparaitre ici une wishlist
-        ";
+          
+        echo "<div id='laWishlist'>";
+        while ($row = mysqli_fetch_assoc($result_wishlist)) {
+            echo "<div class='unWish'>";
+            echo "<span>" . $row["titre"] ." ". "</span>";
+            echo "<span>" . $row["resume"] ." ". "</span>";
+            echo "<span>" . $row["annee_parution"] ." ". "</span>";
+            echo "<span>" . $row["genre"] ." ". "</span>";
+            echo "<span>" . $row["image"] ." ". "</span>";
+            echo "<img src='" . $row["image"] . "' alt='book_pic' style='width: auto;'>";
+            echo "</div>";
+          }
+        echo "</div>";
     } else if ($_GET["type_list"] == "avis") {
-        echo "
-        Vous verrez apparaitre ici des avis
-        ";
+        echo "<div id='lesAvis'>";
+        while ($row = mysqli_fetch_assoc($result_avis)) {
+            echo "<div class='unAvis'>";
+            echo "<span>" . $row["note"] ." ". "</span>";
+            echo "<span>" . $row["commentaire"] ." ". "</span>";
+            echo "</div>";
+          }
+        echo "</div>";
     }
 }
 ?>
