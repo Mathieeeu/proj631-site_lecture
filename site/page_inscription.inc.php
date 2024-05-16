f87a2a737216dd23293b9fb5bf018ec031d97458
Louna Camas <lounacamas@gmail.com>
2024-05-16 10:15:20 +0200

    inscription css

--- site/page_inscription.inc.php
+++ site/page_inscription.inc.php
@@ -1,9 +1,27 @@
-<?php
-    echo "<div class='containter'><h1>Inscription</h1>";
-    echo "<form action='?page=accueil' method='post'>";
-    echo "<input type='text' name='id' placeholder='Identifiant' required><br/>";
-    echo "<input type='password' name='mdp1' placeholder='Mot de passe' required><br>";
-    echo "<input type='password' name='mdp2' placeholder='Confirmer mot de passe' required><br>";
-    echo "<input type='submit' value='Valider'>";
-    echo "</div>";
-?>
+<head>
+<link rel="stylesheet" type="text/css" href="css/page_inscription.css">
+</head>
+
+<body>
+
+<div id=principal>
+
+  <div id=requete>
+    <div> 
+      <h2 id= textprincipal>Inscription</h2>
+    </div> 
+
+    <div class="container">
+    <?php
+        echo "<form action='?page=accueil' method='post'>";
+        echo "<input class= champRecherche type='text' name='id' placeholder='Identifiant' required><br/>";
+        echo "<input class= champRecherche type='password' name='mdp1' placeholder='Mot de passe' required><br>";
+        echo "<input class= champRecherche type='password' name='mdp2' placeholder='Confirmer mot de passe' required><br>";
+        echo "<input class='bouton' type='submit' value='Valider'>";
+    ?>
+
+</div>
+
+</div>
+</div>
+</body>
