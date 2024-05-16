fad508043b0b5b62474e0fe152442095822f8352
Emma Rechon-Reguet <emma.rechon-reguet@inria.fr>
2024-05-16 05:43:08 +0200

    Mise en forme Donnees

---
+++ java/Donnees.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Donnees{
+    private Table tb;
+
+    public Donnees(Table t){
+        this.tb=t;
+    }
+
+    public void modifierDonnee (ArrayList<Integer> valeurs, String nomColonne ){
+
+    }
+
+    public void supprimerDonnee(ArrayList<Integer> valeurs){
+
+    }
+
+    public void afficherDonnee(){
+        
+    }
+}
