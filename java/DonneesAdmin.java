2a4a0970e3bdb00bc0938bba6de28d8b6a892fb2
Emma Rechon-Reguet <emma.rechon-reguet@inria.fr>
2024-05-16 05:50:33 +0200

    Mise en forme DonneesAdmin

---
+++ java/DonneesAdmin.java
@@ -0,0 +1,16 @@
+public class DonneesAdmin extends Donnees{
+    public static int largeur;
+    public static int longueur;
+
+    public DonneesAdmin(Table t){
+        super(t);
+    }
+
+    public void resizeImage(){
+
+    }
+
+    public void AjouterValeurs (ArrayList <String> contenu){
+
+    }
+}
